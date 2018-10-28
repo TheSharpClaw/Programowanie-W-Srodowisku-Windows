48250e1b069845e0ad95f90a51ad7f0ba69268fd
TheSharpClaw <thesharpclaw@gmail.com>
2018-10-28 22:17:18 +0100

    First commit

---
+++ ProgWSrodWin/ProgWSrodWin/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ProgWSrodWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(59, 12);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Location = new System.Drawing.Point(12, 41);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(168, 90);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(192, 143);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ ProgWSrodWin/ProgWSrodWin/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ProgWSrodWin
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort = new SerialPort("COM3", 300, Parity.None, 8, StopBits.One);
+        private byte[] _message = { 0xAA };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _serialPort.WriteTimeout = 2000;
+            _serialPort.ReadTimeout = 2000;
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            _serialPort.Open();
+
+            _serialPort.Write(_message, 0, _message.Length);
+            //textBox.Text = _serialPort.ReadByte().ToString();
+
+            _serialPort.Close();
+        }
+    }
+}
